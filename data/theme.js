b419c2bd863778a292fa275e8c83da3d24b97e03
branzfamily01 <branz.family.01@gmail.com>
2026-08-16 20:58:42 +0900

    Add sea theme

---
+++ data/theme.js
@@ -0,0 +1,15 @@
+window.APP_THEME = {
+  id: 'sea',
+  name: '海の生き物',
+  appTitle: '海のちずパズル',
+  rewardEmoji: '🐚',
+  celebrateEmoji: ['✨','🫧','⭐','🐚','💫'],
+  resultCreatures: ['🐬','🦦','🐧','🐢','🪼'],
+  colors: {
+    ocean: '#34bde7',
+    oceanDeep: '#128ec5',
+    aqua: '#80e8e2',
+    accent: '#ff8ca8'
+  },
+  soundProfile: 'sea-pop'
+};
