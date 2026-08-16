ea125db1b8ca6f3b63896fd73e87c1292632daaa
branzfamily01 <branz.family.01@gmail.com>
2026-08-16 20:58:54 +0900

    Add service worker

---
+++ sw.js
@@ -0,0 +1,13 @@
+const CACHE='umi-no-chizu-puzzle-v1';
+const ASSETS=[
+  './','./index.html','./styles.css','./app.js','./data/theme.js','./data/geography.js','./manifest.json',
+  './assets/japan-prefectures.png','./assets/sea-hero.png','./assets/icons/icon-192.png','./assets/icons/icon-512.png'
+];
+self.addEventListener('install',e=>e.waitUntil(caches.open(CACHE).then(c=>c.addAll(ASSETS)).then(()=>self.skipWaiting())));
+self.addEventListener('activate',e=>e.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(k=>k!==CACHE).map(k=>caches.delete(k)))).then(()=>self.clients.claim())));
+self.addEventListener('fetch',e=>{
+  if(e.request.method!=='GET')return;
+  e.respondWith(caches.match(e.request).then(hit=>hit||fetch(e.request).then(res=>{
+    const copy=res.clone(); caches.open(CACHE).then(c=>c.put(e.request,copy)); return res;
+  }).catch(()=>caches.match('./index.html'))));
+});
